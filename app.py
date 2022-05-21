ab7068b4ae9e41f87bcd02d0aa06c5196155a431
ditavares10 <104534813+ditavares10@users.noreply.github.com>
2022-05-21 09:12:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect(app) 
 
 @app.route("/")
 def pagina_inicial():
-    return "DevOps LAB"
+    return "LabDevOps - Turma 05"
 
 if __name__ == '__main__':
     app.run()
