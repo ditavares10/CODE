dd305be35136fb455a9b82b03f5ded2567bc7aab
ditavares10 <104534813+ditavares10@users.noreply.github.com>
2022-05-21 01:43:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+
+csrf = CSRFProtect(app)                                                                                                                           
+
 
 @app.route("/")
 def pagina_inicial():
     return "DevOps LAB"
 
+@app.route("/")
+def pagina_inicial():
+    return "Laboratório Pipeline DevOps"
+
 if __name__ == '__main__':
     app.run()
+
+
